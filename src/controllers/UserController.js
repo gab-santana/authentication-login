8edd0ad9942acf2de9ca5ce5ab742c2921ec7cde
gab-santana <gabrielsantana100@hotmail.com>
2022-06-03 17:11:26 -0300

    Encrypt password

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,7 +6,6 @@ class UserController {
   async create(req, res) {
     const { name, email, username, password, phone } = req.body;
     const passwordCrypt = await hash(password,10);
-    console.log(passwordCrypt);
     
     const user = await User.create({
       name,
